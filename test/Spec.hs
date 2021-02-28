ba750473ec6fd43ae519e038663306bdaa5b4e0b
Adrian Gogolan <gogolan.adrian@gmail.com>
2021-02-28 13:13:31 +0100

    Red tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,23 +8,24 @@ main = hspec $ do
   -- inspiration from https://www.ibm.com/cloud/architecture/content/course/test-driven-development/tdd-palindrome-kata?task=1
   describe "Lib.palindrome" $ do
     it "returns True for mom" $ do
-      pending
+      palindrome "mom" `shouldBe` True
 
     it "returns False for dude" $ do
-      pending
+      palindrome "dude" `shouldBe` False
 
     it "returns True for mom mom" $ do
-      pending
+      palindrome "mom mom" `shouldBe` True
 
     it "returns False for dad mom" $ do
-      pending
+      palindrome "dad mom" `shouldBe` False
 
     it "returns True for whitespace" $ do
-      pending
+      palindrome " " `shouldBe` True
 
     it "returns error for empty string" $ do
-      pending
+      evaluate (palindrome "") `shouldThrow` anyException
 
     it "returns error for not a string" $ do
-      pending
+      pendingWith "how can i write this?"
+      --evaluate (palindrome 1) `shouldThrow` errorCall
 
